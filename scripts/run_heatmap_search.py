a9e09bfdf6fba915aeb95e4739c8b3ecd58a82ca
HongrooT02 <sgn09018@snu.ac.kr>
2025-08-20 17:49:09 +0900

    logspace search for heatmap

--- scripts/run_heatmap_search.py
+++ scripts/run_heatmap_search.py
@@ -14,8 +14,8 @@ import math
 # ==============================================================================
 
 # --- Training Configuration ---
-betas = [0.01, 0.05, 0.1, 0.5, 1.0, 5.0, 10.0]
-lrs = [0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1]
+betas = np.logspace(-2, 1, num=6)
+lrs = np.logspace(-5, 0, num=10)
 sconda_env_name = "myenv"
 run_name_prefix = "heatmap-test-"
 NETWORK_DEPTH_L = 4  # Number of hidden layers from ep_mnist.yaml
